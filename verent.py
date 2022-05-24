a4f1caf44983ddabc0f83e81f29a94a827ec396a
FatihAhmadZakaria <99313840+FatihAhmadZakaria@users.noreply.github.com>
2022-05-24 09:16:13 +0700

    Add files via upload

--- verent.py
+++ verent.py
@@ -68,7 +68,7 @@ def sewa():
     noktp = input("Masukan NO KTP\n")
     nohp = input("Masukan NO HP\n")
     kd_k = input("Masukan Kode Kendaraan\n")
-    durasi = int(input("Masukan Durasi Sewa\n"))
+    durasi = int(input("Masukan Durasi Sewa /jam\n"))
     stat = "Tidak Tersedia"
     cur.execute("INSERT INTO sewa (nama, noktp, nohp, kd_k, durasi, status) VALUES (%s,%s,%s,%s,%s,%s)",(nama, noktp, nohp, kd_k, durasi, stat))
     mydb.commit()
@@ -82,9 +82,18 @@ def pengembalian():
 def tampilk():
     cur.execute("select * from kendaraan")
     db = cur.fetchall()
-    print("="*15,"Daftar Kendaraan","="*15)
+    print("="*39,"Daftar  Kendaraan","="*39)
+    print('|\t ID\t Nama    \t Plat\t\t Jenis\t Harga      \t Status      \t\t|')
+    print('-'*97)
     for row in db:
-        print(row)
+        print('|\t',row[0],'\t',row[1],'    \t',row[2],'\t',row[3],'\t',row[4],'  \t',row[5],'      \t|')
+    print('='*97)
+
+def tampils():
+    cur.execute("select nama,kd_k from sewa")
+    db = cur.fetchall()
+    for row in db:
+        print (row)
 
 print('='*10,"Selamat Datang di VeRent",'='*10)
 while True:
@@ -94,6 +103,7 @@ while True:
         print("3. Sewa Kendaraan")
         print("4. Pengembalian")
         print("5. Tampilkan Kendaraan")
+        print("6. Tampilkan Penyewa")
         print("0. Keluar")
         pilihan = input("\n-->Masukan Pilihan : ")
         if pilihan == '1':
@@ -106,6 +116,8 @@ while True:
             pengembalian()
         elif pilihan == '5':
             tampilk()
+        elif pilihan == '6':
+            tampils()
         elif pilihan == '0':
             print("\nTerimakasih ^^\n")
             break
